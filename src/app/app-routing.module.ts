5f54190f08a7d292b42c2957a7834be5fdddc310
baypp <pphouone@gmail.com>
2021-03-19 11:24:34 +0100

    router

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,25 @@
+import { AdminOrdersComponent } from './admin/admin-orders/admin-orders.component';
+import { AdminProductsComponent } from './admin/admin-products/admin-products.component';
+import { LoginComponent } from './login/login.component';
+import { OrderSuccessComponent } from './order-success/order-success.component';
+import { CheckOutComponent } from './check-out/check-out.component';
+import { ShoppingCartComponent } from './shopping-cart/shopping-cart.component';
+import { ProductsComponent } from './products/products.component';
+import { HomeComponent } from './home/home.component';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component: HomeComponent},
+  {path: 'products', component: ProductsComponent},
+  {path: 'shopping-cart', component: ShoppingCartComponent},
+  {path: 'check-out', component: CheckOutComponent},
+  {path: 'order-success', component: OrderSuccessComponent},
+  {path: 'login', component: LoginComponent},
+  {path: 'admin/products', component: AdminProductsComponent},
+  {path: 'admin/orders', component: AdminOrdersComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
