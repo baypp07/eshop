d37c86f7d972d1d128c2afe3df6831f3e42d2315
baypp <pphouone@gmail.com>
2021-03-19 16:23:29 +0100

    authentification
    auth-firebase with google account

--- src/app/bs-navbar/bs-navbar.component.ts
+++ src/app/bs-navbar/bs-navbar.component.ts
@@ -1,15 +1,25 @@
+import { AngularFireAuth } from 'angularfire2/auth';
 import { Component, OnInit } from '@angular/core';
+import * as firebase from 'firebase';
 
 @Component({
   selector: 'bs-navbar',
   templateUrl: './bs-navbar.component.html',
   styleUrls: ['./bs-navbar.component.css']
 })
-export class BsNavbarComponent implements OnInit {
+export class BsNavbarComponent {
 
-  constructor() { }
+  user: firebase.User;
+
+  constructor(private afAuth: AngularFireAuth) { 
+    afAuth.authState.subscribe(user => this.user = user ); //authstate: observable of firebase user
+  }
 
   ngOnInit() {
+  }
+  logout(){
+    this.afAuth.auth.signOut();
+
   }
 
 }
