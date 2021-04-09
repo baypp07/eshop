a60096fa785a6b4938d1ac9f584e984452705e99
baypp <pphouone@gmail.com>
2021-04-09 11:59:34 +0200

    Update product-form.component.ts
    apply get(id)//take(1)

--- src/app/admin/product-form/product-form.component.ts
+++ src/app/admin/product-form/product-form.component.ts
@@ -1,7 +1,8 @@
-import { Router } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 import { ProductService } from './../../services/product.service';
 import { CategoryService } from './../../services/category.service';
 import { Component, OnInit } from '@angular/core';
+import 'rxjs/add/operator/take';
 
 @Component({
   selector: 'app-product-form',
@@ -9,13 +10,18 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./product-form.component.css']
 })
 export class ProductFormComponent implements OnInit {
-  categories$
+  categories$;
+  product = {};
   constructor(
     private router: Router,
+    private route: ActivatedRoute,
     private categoryService: CategoryService,
     private productService: ProductService,
+
   ) {
     this.categories$= categoryService.getCategories();
+    let id = this.route.snapshot.queryParamMap.get('id');
+    if(id) this.productService.get(id).take(1).subscribe(p=> this.product = p);
    }
 
    save(product){
