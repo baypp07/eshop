5afdc769673baecc63ad5b94d6d56121688a15d2
baypp <pphouone@gmail.com>
2021-04-11 13:23:20 +0200

    update datablemodule
    run npm i angular5-data-table --save

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,6 +14,8 @@ import { AngularFireDatabaseModule} from 'angularfire2/database';
 import { AngularFireAuthModule} from 'angularfire2/auth';
 import { FormsModule} from '@angular/forms';
 import { CustomFormsModule} from 'ng2-validation';
+import { DataTableModule} from 'angular5-data-table';
+
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -55,6 +57,7 @@ import { ProductFormComponent } from './admin/product-form/product-form.componen
     NgbModule,
     FormsModule,
     CustomFormsModule,
+    DataTableModule,
   ],
   providers: [
     AuthService,
