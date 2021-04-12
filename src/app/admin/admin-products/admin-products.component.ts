cdabb1b7cc336b7aa37cd8e553d8bebaad127713
baypp <pphouone@gmail.com>
2021-04-12 02:47:01 +0200

    update

--- src/app/admin/admin-products/admin-products.component.ts
+++ src/app/admin/admin-products/admin-products.component.ts
@@ -2,6 +2,8 @@ import { Product } from './../../models/product';
 import { ProductService } from './../../services/product.service';
 import { Component, OnDestroy, OnInit } from '@angular/core';
 import { Subscription } from 'rxjs';
+import { DataTableResource } from 'angular5-data-table';
+
 
 @Component({
   selector: 'app-admin-products',
@@ -12,12 +14,29 @@ export class AdminProductsComponent implements OnInit, OnDestroy {
   products : Product[];
   filteredProducts : any[];
   subscription:Subscription;
+  tableResource:DataTableResource<Product>;
+  items:Product[] = [];
+  itemCount:number;
 
   constructor(private productService: ProductService) {
 
     this.subscription = this.productService.getAll()
-    .subscribe(products => this.filteredProducts = this.products = products);
+    .subscribe(products => {this.filteredProducts = this.products = products;
+    this.initializeTable(products);})
+
    }
+   private initializeTable(products: Product[]){
+     this.tableResource = new DataTableResource(products);
+     this.tableResource.query({offset:0}).then(items => this.items = items);
+     this.tableResource.count().then(count => this.itemCount = count);
+   }
+
+   reloadItems(params){
+     if (!this.tableResource)return;
+     this.tableResource.query(params).then(items => this.items = items);
+     
+   }
+
 
    filter(query:string){
      this.filteredProducts = query ?
