b4a4e118bd15f44de107232ba70e78fdaf71eb90
baypp <pphouone@gmail.com>
2021-04-12 13:51:25 +0200

    active class by selecting category

--- src/app/products/products.component.ts
+++ src/app/products/products.component.ts
@@ -1,6 +1,9 @@
+import { Product } from './../models/product';
 import { CategoryService } from './../services/category.service';
 import { ProductService } from './../services/product.service';
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import 'rxjs/add/operator/switchMap';
 
 @Component({
   selector: 'app-products',
@@ -8,13 +11,31 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./products.component.css']
 })
 export class ProductsComponent {
-  products$;
-  categories$
+  products: Product[] = [];
+  filteredProducts: Product[] = [];
+  category: string;
+  categories$;
 
-  constructor(productservice:ProductService, categoryService:CategoryService) {
-    this.products$ = productservice.getAll();
+  constructor(
+    route:ActivatedRoute,
+    productService:ProductService,
+    categoryService:CategoryService
+    ) {
+
+    productService
+    .getAll()
+    .switchMap((products:Product[]) => {
+      this.products = products;
+      return route.queryParamMap;
+    })
+    .subscribe(params => {
+      this.category = params.get('category');
+
+      this.filteredProducts = (this.category) ? 
+      this.products.filter(p => p.category === this.category):
+      this.products;
+    });
     this.categories$ = categoryService.getAll();
-    
    }
 
   ngOnInit() {
