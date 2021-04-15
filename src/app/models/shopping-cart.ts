079121a8fd841415ad526b9fbf83fc778f4549fb
baypp <pphouone@gmail.com>
2021-04-15 17:34:58 +0200

    fixing

--- src/app/models/shopping-cart.ts
+++ src/app/models/shopping-cart.ts
@@ -1,9 +1,13 @@
 import { ShoppingCartItem } from './shopping-cart-item';
 
 export class ShoppingCart{
+    items:ShoppingCartItem[]=[];
     constructor(
-        public items:ShoppingCartItem[]){}
-        
+        public itemsMap : {[productId:string]: ShoppingCartItem}){
+            for(let productId in itemsMap)
+            this.items.push(itemsMap[productId]);
+        }
+
     get totalItemCount(){
         let count = 0;
         for (let productId in this.items)
